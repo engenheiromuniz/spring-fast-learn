f224626b8d2bb898458d194f424fe06cc745a338
Engenheiro Muniz <engenheiro.muniz@outlook.com>
2026-09-08 11:24:18 -0300

    chore: setup inicial do ms-cliente com Spring Boot, JPA, H2 e Validation

---
+++ spring_fast_learn/src/main/java/com/mztech/spring_fast_learn/SpringFastLearnApplication.java
@@ -0,0 +1,13 @@
+package com.mztech.spring_fast_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFastLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFastLearnApplication.class, args);
+	}
+
+}
---
+++ spring_fast_learn/src/test/java/com/mztech/spring_fast_learn/SpringFastLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mztech.spring_fast_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFastLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
